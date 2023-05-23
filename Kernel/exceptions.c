3738f27dc02aa6eb60875047df1d6dc26b52547d
tm-sm <84805291+tm-sm@users.noreply.github.com>
2023-05-23 15:38:28 -0300

    code format tweaks

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,9 +1,14 @@
 #include <stdint.h>
 #include <console.h>
+
 #define ZERO_EXCEPTION_ID 0
 #define INVALIDO_OP_ID 6
+
 static char* regs[]={"rax:","rbx:","rcx:","rdx:","rsi:","rdi:","rbp:","r8:","r9:","r10:","r11:","r12:","r13:","r14:","r15:","rsp:","rip"};
+
 static void zero_division();
+static void opinvalid(uint64_t regs, uint64_t* rip);
+
 void printregs(uint64_t* exregs){
 	for(int i=0;i<17;i++){
 		cPrint(regs[i]);
