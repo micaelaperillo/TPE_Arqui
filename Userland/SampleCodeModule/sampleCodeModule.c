617dbec5083ecc07eb2abc6bcd9d68945b665d9e
tm-sm <84805291+tm-sm@users.noreply.github.com>
2023-05-25 14:15:34 -0300

    removes unnecessary lines

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -2,11 +2,6 @@
 #include <stdint.h>
 #include <standardLib.h>
 
-char * v = (char*)0xB8000 + 79 * 2;
-
-static int var1 = 0;
-static int var2 = 0;
-
 int main() {
 
     //DIBUJA UN CUADRADO EN LA PANTALLA CON UNA SYS_CALL COMO EJEMPLO
