84170f219ba0e105d2a237b15b50b05a33373862
tm-sm <84805291+tm-sm@users.noreply.github.com>
2023-05-27 14:05:29 -0300

    adds time.h to the user space

--- Userland/SampleCodeModule/include/time.h
+++ Userland/SampleCodeModule/include/time.h
@@ -1,4 +1,13 @@
 #ifndef TPE_ARQUI_TIME_H
 #define TPE_ARQUI_TIME_H
 
+unsigned int getSeconds();
+unsigned int getMinutes();
+unsigned int getHours();
+unsigned int getDay();
+unsigned int getMonth();
+unsigned int getYear();
+void printTime();
+void printDate();
+
 #endif //TPE_ARQUI_TIME_H
--- Userland/SampleCodeModule/time.c
+++ Userland/SampleCodeModule/time.c
@@ -1,3 +1,51 @@
-//
-// Created by timos on 27/5/2023.
-//
+#include <stdint.h>
+#include <standardLib.h>
+
+#define SYS_GET_TIME_ID 6
+
+enum TimeID {
+    SECONDS,
+    MINUTES,
+    HOURS,
+    DAY,
+    MONTH,
+    YEAR
+};
+
+unsigned int retrieve(int id) {
+    unsigned int* ptr = 0;
+    interrupt(SYS_GET_TIME_ID, id, *ptr, 0, 0, 0);
+    return *ptr;
+}
+
+unsigned int getSeconds() {
+    return retrieve(SECONDS);
+}
+
+unsigned int getMinutes() {
+    return retrieve(MINUTES);
+}
+
+unsigned int getHours() {
+    return retrieve(HOURS);
+}
+
+unsigned int getDay() {
+    return retrieve(DAY);
+}
+
+unsigned int getMonth() {
+    return retrieve(MONTH);
+}
+
+unsigned int getYear() {
+    return retrieve(YEAR);
+}
+
+void printTime() {
+    printFormat("%d:%d:%d", getHours(), getMinutes(), getSeconds());
+}
+
+void printDate() {
+    printFormat("%d/%d/%d", getDay(), getMonth(), getYear());
+}
