a05ef740cc72133bb1bfb12af6e9376df4501891
Mica Perillo <micaelaperillo26@gmail.com>
2023-05-29 08:16:40 -0300

    added computer movement

--- Userland/SampleCodeModule/pong.c
+++ Userland/SampleCodeModule/pong.c
@@ -95,25 +95,33 @@ void move_ball(ball * b) {
 
 } 
 
-void handle_moves(bar * b) {
+void handle_moves(game g) {
     // gets key with the keyboard driver
-    // if key == W --> move_bar(&bar, -1)
-    // if key == S --> move_bar(&bar, 1)
+    // if key == W --> move_bar(bar, -1)
+    // if key == S --> move_bar(bar, 1)
     // else --> no hace nada
 
     char c = getChar();
     if (c == "w" || c == "W") {
-        move_bar(*b, -1);
+        move_bar(g.user.v_bar, -1);
     } else if (c == "s" || c == "S") {
-        move_bar(*b, 1);
+        move_bar(g.user.v_bar, 1);
     }
+
+    // when the user bar moves, the computer does too, depending on the ball's position
+    if (g.ball.y < SCREEN_HEIGHT/2) {
+        move_bar(g.computer.v_bar, -1);
+    } else {
+        move_bar(g.computer.v_bar, 1);
+    }
+
 }
 
 void move_bar(bar b, uint32_t y) {
 
     // draws black rectangle on top of the old bar
     drawRectangle(0x00, b.x, b.y, BAR_WIDTH, BAR_HEIGHT);
-    b.y+= y;
+    b.y += y;
     drawRectangle(0xFF, b.x, b.y, BAR_WIDTH, BAR_HEIGHT);
 
 
@@ -168,21 +176,25 @@ void pong() {
         init_draw(game);
 
         // TODO: no estoy muy segura de la logica del handle_moves y move_ball, no se si estan en el orden correcto
-        handle_moves(&user.v_bar);
-        move_ball(&game.ball);
+        int finished_point = 0;
 
-        if (game.ball.x == game.user.v_bar.x) {
-            if (!ball_touches_bar(game.ball, game.user.v_bar)) {
-                // point for computer
-                game.computer.score++;
-                init_draw(game);
+        while (!finished_point) {
+            handle_moves(game);
+            move_ball(&game.ball);
+
+            if (game.ball.x == game.user.v_bar.x) {
+                if (!ball_touches_bar(game.ball, game.user.v_bar)) {
+                    // point for computer
+                    game.computer.score++;
+                    finished_point = 1;
+                }
             }
-        }
-        if (game.ball.x == game.computer.v_bar.x) {
-            if (!ball_touches_bar(game.ball, game.computer.v_bar)) {
-                // point for user
-                game.user.score++;
-                init_draw(game);
+            if (game.ball.x == game.computer.v_bar.x) {
+                if (!ball_touches_bar(game.ball, game.computer.v_bar)) {
+                    // point for user
+                    game.user.score++;
+                    finished_point = 1;
+                }
             }
         }
     }
