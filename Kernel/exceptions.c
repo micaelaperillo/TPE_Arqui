7852d116d6854ce05a5718a2c6d20d5b018a9174
Ncasella <nicolascasella02@gmail.com>
2023-06-02 20:16:51 -0300

    ultimo fix a exceptions

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -35,7 +35,6 @@ static void exScreen(char* str, uint64_t exregs,uint64_t* rip,uint64_t *rsp) {
 	int i=0;
 	//cPrint("esta terminal explotara en 5 segundos\n"); //todo:ponerle un timer o algo para que quede mas lindo la lanzada de exception
 	*rip=0x400000; //direccion del sample code module
-	*rsp=getStackBase()-10; // restora el stack pointer al volver de la exepcion
-
+	*rsp=getStackBase()-20; // restora el stack pointer al volver de la exepcion
 }
 
