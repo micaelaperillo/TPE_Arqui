3a59729dc4ceef039686150508e64b44ca7cd81a
Mica Perillo <micaelaperillo26@gmail.com>
2023-06-04 14:49:36 -0300

    added comments in time.h and utils.h

--- Kernel/include/utils.h
+++ Kernel/include/utils.h
@@ -2,6 +2,7 @@
 #define TPE_ARQUI_UTILS_H
 #include <stdint.h>
 
+// converts uint to specified base
 uint32_t uintToBase(uint64_t value, char * buffer, uint32_t base);
 
 #endif //TPE_ARQUI_UTILS_H
