1a636bf45b64cdcfc059e72a3788637006138deb
Mica Perillo <micaelaperillo26@gmail.com>
2023-06-04 14:46:29 -0300

    add comments and better formatting to naiveConsole

--- Kernel/naiveConsole.c
+++ Kernel/naiveConsole.c
@@ -7,55 +7,42 @@ static uint8_t * currentVideo = (uint8_t*)0xB8000;
 static const uint32_t width = 80;
 static const uint32_t height = 25 ;
 
-void ncPrint(const char * string)
-{
-	int i;
-
-	for (i = 0; string[i] != 0; i++)
+void ncPrint(const char * string) {
+	for (int i = 0; string[i] != 0; i++)
 		ncPrintChar(string[i]);
 }
 
-void ncPrintChar(char character)
-{
+void ncPrintChar(char character) {
 	*currentVideo = character;
 	currentVideo += 2;
 }
 
-void ncNewline()
-{
-	do
-	{
+void ncNewline() {
+	do {
 		ncPrintChar(' ');
 	}
 	while((uint64_t)(currentVideo - video) % (width * 2) != 0);
 }
 
-void ncPrintDec(uint64_t value)
-{
+void ncPrintDec(uint64_t value) {
 	ncPrintBase(value, 10);
 }
 
-void ncPrintHex(uint64_t value)
-{
+void ncPrintHex(uint64_t value) {
 	ncPrintBase(value, 16);
 }
 
-void ncPrintBin(uint64_t value)
-{
+void ncPrintBin(uint64_t value) {
 	ncPrintBase(value, 2);
 }
 
-void ncPrintBase(uint64_t value, uint32_t base)
-{
+void ncPrintBase(uint64_t value, uint32_t base) {
     uintToBase(value, buffer, base);
     ncPrint(buffer);
 }
 
-void ncClear()
-{
-	int i;
-
-	for (i = 0; i < height * width; i++)
+void ncClear() {
+	for (int i = 0; i < height * width; i++)
 		video[i * 2] = ' ';
 	currentVideo = video;
 }
